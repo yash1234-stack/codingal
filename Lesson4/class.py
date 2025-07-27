a901236f07801db535bc4115a30e125b783c1f5d
yash1234-stack <yashurmom88@gmail.com>
2025-07-27 19:27:49 +0530

    classwork

---
+++ Lesson4/class.py
@@ -0,0 +1,27 @@
+s1=40.0
+s2=70.0
+s3=50.0
+s4=60.0
+
+avg = (s1+s2+s3+s4)/4.0
+print(avg)
+
+#q2
+i=0
+sum=0
+tree = [98.0, 94.0, 41.0, 96.0, 10.0]
+for i in range(5):
+    sum = sum+tree[i]
+avg = sum/5.0
+print(avg)
+
+#3
+Notes = [500, 200, 100, 50, 20, 10]
+
+a=input("enter amount")
+a=int(a)
+sum=0
+for i in range(len(Notes)):
+    sum=a//Notes[i]
+    a%=Notes[i]
+    print(f"{Notes[i]} is {sum}")
