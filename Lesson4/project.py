ca4fc684ceccf167f8601d66a6a2fe5ed72d575f
yash1234-stack <yashurmom88@gmail.com>
2025-08-03 19:01:42 +0530

    lesson 5 class

--- Lesson4/project.py
+++ Lesson4/project.py
@@ -1,13 +1,13 @@
-# n=int(input("enter number"))
-# t=False
-# for i in range(n):
-#     if(i**2.0==n):
-#         t=True
-#         break
-# if t==True:
-#     print(f"the square root is {i}")
-# else:
-#     print("the number is not a perfect square")
+n=int(input("enter number"))
+t=False
+for i in range(n):
+    if(i**2.0==n):
+        t=True
+        break
+if t==True:
+    print(f"the square root is {i}")
+else:
+    print("the number is not a perfect square")
 
 
 a = int(input("enter number "))
---
+++ Lesson5/class.py
@@ -0,0 +1,35 @@
+# a  = int(input("enter your number "))
+
+# if a>=0:
+#     print("+ve")
+# else:
+#     print("-ve")
+
+
+# cp = int(input("enter cp "))
+# sp = int(input("enter sp "))
+
+# if cp>sp:
+#     print(f"loss of{cp-sp} or {((cp-sp)/cp)*100}")
+# elif cp==sp:
+#     print("breakeven")
+# else:
+#     print(f"profit of{sp-cp} or {((sp-cp)/cp)*100}%")
+
+# a = int(input("enter number to check if it is ggreater than 15 "))
+# if a>15:
+#     print("greater")
+# elif a==15:
+#     print("equal")
+# else: 
+#     print("lesser")
+
+
+a  = int(input("enter your number "))
+
+if a%2== 0:
+    print("even")
+elif a==0:
+    print("zero")
+else:
+    print("odd")
